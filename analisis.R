73170e784157e192fc7b73cd0321b90cc3d1e522
irrubio <iratxe.rubio@bc3research.org>
2021-01-30 22:40:40 +0100

    final

--- analisis.R
+++ analisis.R
@@ -2,6 +2,7 @@
 # install.packages("ggplot2")
 
 library(tidyverse) #incluye ggplot2 y otros 
+library(readxl) #read_xlsx
 
 # TODO: Ajustar la ruta según corresponda
 ruta.dataset <- "<DIRECTORIO_DEL_DATASET>/dataset_final.csv"
@@ -12,7 +13,7 @@ df <- read.csv("dataset_final.csv")
 
 # Ignorar edades
 ignorar.edades <- c("De 16 a 19 años", "De 65 a 69 años", "70 Y Más Años")
-aEliminar <- which(df$Edad %in% ignorar.edades) #me daba vacío
+aEliminar <- which(df$Edad %in% ignorar.edades) 
 df <- df[-aEliminar,] 
 
 # Transformar la edad a <<factor>> para ordenar las gráficas
@@ -49,8 +50,8 @@ colnames(df.ocupados)[4] <- "Ocupados"
 df.ratio <- merge(df.activos, df.ocupados, by = c("Trimestre", "Edad", "Sexo"))
 df.ratio$Ratio <- df.ratio$Ocupados / df.ratio$Activos
 
-color.hombres <- "#56B4E9"
-color.mujeres <- "#F8917E"
+color.hombres <- "limegreen"
+color.mujeres <- "purple1"
 colors.vector <- c(color.hombres, color.mujeres)
 
 #******************************************************************************#
@@ -103,11 +104,10 @@ labels.trimestre <- c("1" = 2010,
                       "41" = 2020)
 
 ggplot(data = df.ratio, aes(x = TrimestreAsNum, y = Ratio, group = Sexo)) +
-  geom_line(aes(color = Sexo))+
+  geom_line(aes(color = Sexo), size = 1)+
   facet_wrap(~Edad, scales="free") +
   xlab(NULL) + 
   ylab(NULL) + 
-  #ggtitle("Tasa de ocupación") + #Quito esta línea porque debería de ir explicado en el pie de figura
   coord_cartesian(ylim = c(0.4, 1)) +
   scale_color_manual(values = colors.vector) +
   scale_x_discrete(breaks = seq(1, total.trimestres, trimestres.intervalo.size),
@@ -126,10 +126,50 @@ ggplot(data = df.ratio, aes(x = TrimestreAsNum, y = Ratio, group = Sexo)) +
 #*         Comparación de tasa de ocupación de las mujeres por edad           *#
 #******************************************************************************#
 df.mujeres <- df.ratio[df.ratio$Sexo == "Mujeres",]
+
+df.mujeres$year <- substr(df.mujeres$Trimestre, 1, 4)
+df.mujeres$year <- as.numeric(df.mujeres$year)
+
 # Diagrama de cajas y bigotes
 ggplot(data = df.mujeres, aes(x=Edad, y = Ratio)) + 
   geom_boxplot(fill = color.mujeres) +
   xlab(NULL) + ylab(NULL) + ggtitle("Tasa de ocupación de mujeres")
+
+df.mujeres.simpl <- df.mujeres %>%
+                      filter(year %in% c(2010, 2020)) %>%
+                      group_by(Edad, year) %>%
+                      summarise(promedio_ratio = mean(Ratio))
+df.mujeres.2010 <- filter(df.mujeres.simpl , year %in% c(2010))
+colnames(df.mujeres.2010 )[3] <-"promedio_ratio_2010"
+df.mujeres.2020 <- filter(df.mujeres.simpl , year %in% c(2020))
+colnames(df.mujeres.2020)[3] <-"promedio_ratio_2020"
+
+###################Lollipop charts
+mujeres.lolipop <- df.mujeres.2010[, -2]
+mujeres.lolipop <- cbind(mujeres.lolipop, df.mujeres.2020[,3])
+mujeres.lolipop$diferencia <- NA
+mujeres.lolipop$diferencia <- mujeres.lolipop$promedio_ratio_2020 - mujeres.lolipop$promedio_ratio_2010
+mujeres.lolipop$Edad_corta <- NA
+mujeres.lolipop$Edad_corta <- substr(mujeres.lolipop$Edad, 4, 15)
+
+ggplot(mujeres.lolipop) +
+  geom_segment(aes(x=Edad_corta, xend=Edad_corta, y=promedio_ratio_2020, yend=promedio_ratio_2010), 
+               color="black", size = 1) +
+  geom_point(aes(x=Edad_corta, y=promedio_ratio_2020, color = "darkblue"), size=5) +
+  geom_point(aes(x=Edad_corta, y=promedio_ratio_2010, color = "lightblue"), size=5) +
+  scale_color_manual(values = c("darkblue", "lightblue"), 
+                     labels = c("2020", "2010")) +
+  theme_classic() +
+  theme(axis.text.x = element_text(size = 16, angle = 50, hjust = 1),
+        axis.text.y = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 16),
+        plot.title = element_text(size = 20)) +
+  xlab("") +
+  ylab("Promedio de la tasa de ocupación-mujer") +
+  ggtitle("b)")
+
 
 #Histograma
 ggplot(df.mujeres, aes(Edad, Ratio)) + 
@@ -139,13 +179,70 @@ ggplot(df.mujeres, aes(Edad, Ratio)) +
   ggtitle("Tasa de ocupación de mujeres") +
   theme_bw() 
 
-df.mujeres$year <- substr(df.mujeres$Trimestre, 1, 4)
-df.mujeres$year <- as.numeric(df.mujeres$year)
+##################Promedio############################
+promedios <- read_xlsx("promedios_anuales.xlsx", sheet = 2)
 
-dos_year <- filter(df.mujeres, year %in% c(2010, 2020))
+scaleFUN <- function(x) sprintf("%.0f", x)
 
+ggplot(promedios, aes(year, promedio_mujeres, color = color.mujeres)) +
+  geom_line(size = 1) +
+  geom_line(aes(year, promedio_hombres, color = color.hombres), size = 1) +
+  scale_color_manual(values = c(color.hombres, color.mujeres), labels = c("Hombre", "Mujer")) +
+  ylab("Promedio") +
+  xlab(" ") +
+  theme_bw() + 
+  scale_x_continuous(labels = scaleFUN) +
+  theme(axis.text.x = element_text(size = 16),
+        axis.text.y = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 16))
+
+
+##################Brecha############################
+promedios <- promedios %>%
+              mutate(brecha = (promedio_hombres - promedio_mujeres)*100)
+
+ggplot(promedios, aes(year, brecha)) +
+  geom_bar(stat = "identity") +
+  ylab("Brecha (%)") +
+  xlab(" ") +
+  theme_bw() + 
+  scale_x_continuous(labels = scaleFUN) +
+  theme(axis.text.x = element_text(size = 16),
+        axis.text.y = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 16))
+
+###############Lollipop hombres/mujeres
+ggplot(promedios) +
+  geom_segment(aes(x=year, xend=year, y=promedio_hombres, yend=promedio_mujeres), 
+               color="black", size = 1) +
+  geom_point(aes(x=year, y=promedio_hombres, color = color.hombres), size=5) +
+  geom_point(aes(x=year, y=promedio_mujeres, color = color.mujeres), size=5) +
+  scale_color_manual(values = c(color.hombres, color.mujeres), 
+                     labels = c("Hombre", "Mujer")) +
+  scale_x_continuous(labels = scaleFUN) +
+  theme_classic() +
+  theme(axis.text.x = element_text(size = 16),
+        axis.text.y = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 16),
+        plot.title = element_text(size = 20)) +
+  xlab("") +
+  ylab("Promedio de la tasa de ocupación") +
+  ggtitle("a)") 
 
 # Gráfica de la tasa de ocupación de hombres vs tasa de ocupación de mujeres
 # df.hombres <- df.ratio[df.ratio$Sexo == "Hombres",]
 # df.mujeres <- df.ratio[df.ratio$Sexo == "Mujeres",]
 # plot(df.hombres$Ratio, df.mujeres$Ratio)
+
+edad1 <- c("72 %", "77 %")
+edad2 <- c("81 %", "84 %")
+
+d <- data.frame(edad1, edad2)
+colnames(d) <- c("20-29 años", "40-64 años")
+rownames(d) <- c("Mujeres", "Hombres")
